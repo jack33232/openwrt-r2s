8565239636192d2850867b1195da566a088f9c16
jzhuo3 <jack33232@gmail.com>
2021-02-21 15:27:27 +0800

    ...

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone --single-branch -b openwrt-21.02 https://github.com/openwrt/openwrt openwrt
-cd openwrt
+pushd openwrt
 git checkout --detach 6a4dcb4719
+popd
 git clone -b linksys-ea6350v3-mastertrack --depth 1 https://github.com/NoTengoBattery/openwrt NoTengoBattery
 exit 0
